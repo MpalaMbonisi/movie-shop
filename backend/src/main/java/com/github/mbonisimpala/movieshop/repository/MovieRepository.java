7182e8440646d6bdd39ddce70c5c4957788ea8d3
Mpalambonisi <mpalambonisi63@gmail.com>
2025-04-20 15:35:30 +0200

    Modified: Added method to find a list of movies by genre id.

--- backend/src/main/java/com/github/mbonisimpala/movieshop/repository/MovieRepository.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/repository/MovieRepository.java
@@ -3,5 +3,8 @@ package com.github.mbonisimpala.movieshop.repository;
 import com.github.mbonisimpala.movieshop.entity.Movie;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends CrudRepository<Movie, Long> {
+    List<Movie> findByGenreId(Long genreId);
 }
