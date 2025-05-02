9daaebc27eba7276609c6907f66d88a58ce531e0
Mpalambonisi <mpalambonisi63@gmail.com>
2025-05-02 13:23:34 +0200

    Added secret key to the SECRET_KEY constant. HS512 bit string.

--- backend/src/main/java/com/github/mbonisimpala/movieshop/security/SecurityConstants.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/security/SecurityConstants.java
@@ -1,7 +1,8 @@
 package com.github.mbonisimpala.movieshop.security;
 
 public class SecurityConstants {
-    public static final String SECRET_KEY = "";
+    public static final String SECRET_KEY = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALtygGpg/DSVC9Zv9gXN3JQQgCCmkpyd" +
+            "/0AZUzqgtrPoYEJTu7z9AdGSwYT+ySEU5FpbANg6xoSbTHHnr+WSUb0CAwEAAQ==";
     public static final int TOKEN_EXPIRATION = 7_200_000; // 2 hours
     public static final String BEARER = "Bearer "; // Authorization : "Bear " + Token
     public static final String AUTHORIZATION = "Authorization"; // "Authorization": Bearer Token
--- backend/src/main/java/com/github/mbonisimpala/movieshop/security/filter/JWTAuthorisationFilter.java
+++
@@ -1,4 +0,0 @@
-package com.github.mbonisimpala.movieshop.security.filter;
-
-public class JWTAuthorizationFilter {
-}
