a1e62b2de6fe74e90a15cdb01b89e0b18d863504
Mpalambonisi <mpalambonisi63@gmail.com>
2025-05-02 02:52:20 +0200

    Update: implemented user authenticate, now the user can be authenticated.

--- backend/src/main/java/com/github/mbonisimpala/movieshop/security/SecurityConfig.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.github.mbonisimpala.movieshop.security;
 
+import com.github.mbonisimpala.movieshop.security.filter.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +13,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        AuthenticationFilter authFilter = new AuthenticationFilter();
+        authFilter.setFilterProcessesUrl("/account/authenticate");
+
         http
                 .headers().frameOptions().disable() // H2 Console runs on a 'frame'. By default Spring Security prevents rendering within an iframe. This line disables its prevention.
                 .and()
@@ -21,6 +26,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilter(authFilter)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
--- backend/src/main/java/com/github/mbonisimpala/movieshop/security/filter/AuthenticationFilter.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/security/filter/AuthenticationFilter.java
@@ -1,4 +1,27 @@
 package com.github.mbonisimpala.movieshop.security.filter;
 
-public class AuthenticationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mbonisimpala.movieshop.entity.Account;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override // request on '/account/authenticate'
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try{
+            Account account = new ObjectMapper().readValue(request.getInputStream(), Account.class);
+            System.out.println(account.getEmail());
+            System.out.println(account.getPassword());
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+
+        return super.attemptAuthentication(request, response);
+    }
+
 }
