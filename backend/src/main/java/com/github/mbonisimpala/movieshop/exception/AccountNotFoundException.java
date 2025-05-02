b4d570110b4ef0189a2989dd0c31048a24e19d46
Mpalambonisi <mpalambonisi63@gmail.com>
2025-05-02 02:16:45 +0200

    Added: global exception handler and implementated exception handling.

--- backend/src/main/java/com/github/mbonisimpala/movieshop/exception/AccountNotFoundException.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/exception/AccountNotFoundException.java
@@ -2,7 +2,7 @@ package com.github.mbonisimpala.movieshop.exception;
 
 public class AccountNotFoundException extends RuntimeException{
 
-    public AccountNotFoundException (long id){
-        super("The account id '" + id + "' does not exist in our records.");
+    public AccountNotFoundException (String email){
+        super("The account with email or id '" + email + "' does not exist in our records.");
     }
 }
