69299f3922196b8b757bd8583e68e59de8c85578
Mpalambonisi <mpalambonisi63@gmail.com>
2025-05-02 13:36:16 +0200

    Finalise the functionality of authenticating the user. Implementating authenticate() method on the CustomAuthenticationManager class.

--- backend/src/main/java/com/github/mbonisimpala/movieshop/security/manager/CustomAuthenticationManager.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/security/manager/CustomAuthenticationManager.java
@@ -1,4 +1,30 @@
 package com.github.mbonisimpala.movieshop.security.manager;
 
-public class CustomAuthenticationManager {
+import com.github.mbonisimpala.movieshop.entity.Account;
+import com.github.mbonisimpala.movieshop.service.AccountService;
+import com.github.mbonisimpala.movieshop.service.AccountServiceImp;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CustomAuthenticationManager implements AuthenticationManager {
+
+    private AccountService accountService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        Account account = accountService.getAccountByEmail(authentication.getName());
+        if(!bCryptPasswordEncoder.matches(authentication.getCredentials().toString(), account.getPassword())){
+            throw new BadCredentialsException("Incorrect Password");
+        }
+        return new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials());
+    }
 }
