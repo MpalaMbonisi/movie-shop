ee013dba205048c059a660aa3c65cb4af7136d69
Mpalambonisi <mpalambonisi63@gmail.com>
2025-05-04 20:04:32 +0200

    Added the GenreNotFoundException to be handled alongside the other exceptions.

--- backend/src/main/java/com/github/mbonisimpala/movieshop/ApplicationExceptionHandler.java
+++ backend/src/main/java/com/github/mbonisimpala/movieshop/ApplicationExceptionHandler.java
@@ -2,7 +2,9 @@ package com.github.mbonisimpala.movieshop;
 
 import com.github.mbonisimpala.movieshop.exception.AccountNotFoundException;
 import com.github.mbonisimpala.movieshop.exception.ErrorResponse;
+import com.github.mbonisimpala.movieshop.exception.GenreNotFoundException;
 import com.github.mbonisimpala.movieshop.exception.MovieNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +21,15 @@ import java.util.List;
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({AccountNotFoundException.class, MovieNotFoundException.class})
+    @ExceptionHandler({AccountNotFoundException.class, MovieNotFoundException.class, GenreNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex){
         ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex){
+        ErrorResponse error = new ErrorResponse(List.of("Cannot delete non-existing resource"));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
